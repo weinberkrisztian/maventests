441087ef725b769c354ec844b1485ccab0886a21
Weinber Krisztián <wkrissz97@gmail.com>
2021-04-25 09:54:07 +0200

    create mvn tests

---
+++ src/main/java/Function.java
@@ -0,0 +1,6 @@
+public class Function {
+
+    public static int terulet(int a, int b){
+     return a*b;
+    }
+}
---
+++ src/test/java/TestFunction.java
@@ -0,0 +1,12 @@
+import org.junit.Test;
+
+import static org.junit.Assert.assertEquals;
+
+public class TestFunction {
+
+    @Test
+    public void test_elso(){
+    assertEquals(21,Function.terulet(7,3));
+        System.out.println("elso teszt siker");
+    }
+}
